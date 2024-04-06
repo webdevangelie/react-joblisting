775b02ea12703523a2771148dfd6c5cfb3e87327
Angelie Sioson <angeliesioson@gmail.com>
2024-04-06 19:32:03 +0800

    implementing browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
      <Route index element={<HomePage/>} />
+     <Route path='/jobs' element={<JobsPage/>} />
+     <Route path='*' element={<NotFound/>} />
   </Route>
   )
 )
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavBar from '../components/Navbar'
 
 const MainLayout = () => {
   return (
     <> 
+        <NavBar />
         <Outlet />
     </>
   );
