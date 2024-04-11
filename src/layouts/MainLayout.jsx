acfcddc82bd945b6bb369ea5dc97ad7556fc02e2
Angelie Sioson <angeliesioson@gmail.com>
2024-04-11 18:24:54 +0800

    implement crud functionality

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import NavBar from '../components/Navbar'
 
 const MainLayout = () => {
@@ -7,6 +9,7 @@ const MainLayout = () => {
     <> 
         <NavBar />
         <Outlet />
+        <ToastContainer />
     </>
   );
 }
--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,24 +1,30 @@
-import { useState} from 'react'
-import { useNavigate } from 'react-router-dom'
-import {toast} from 'react-toastify'
+import {React, useState } from 'react'
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
-const AddJobPage = ({ addJobSubmit}) => {
-    const [title, setTitle] = useState('')
-    const [type, setType] = useState('Full-Time')
-    const [location, setLocation] = useState('')
-    const [description, setDescription] = useState('')
-    const [salary, setSalary] = useState('Under $50K')
-    const [companyName, setCompanyName] = useState('')
-    const [companyDescription, setCompanyDescription] = useState('')
-    const [contactEmail, setContactEmail] = useState('')
-    const [contactPhone, setContactPhone] = useState('')
+const EditJobPage = ( updateJobSubmit ) => {
+
+    const job = useLoaderData();
+
+    const [title, setTitle] = useState(job.title)
+    const [type, setType] = useState(job.type)
+    const [location, setLocation] = useState(job.location)
+    const [description, setDescription] = useState(job.description)
+    const [salary, setSalary] = useState(job.salary)
+    const [companyName, setCompanyName] = useState(job.company.name)
+    const [companyDescription, setCompanyDescription] = useState(job.company.description)
+    const [contactEmail, setContactEmail] = useState(job.company.contactEmail)
+    const [contactPhone, setContactPhone] = useState(job.company.contactPhone)
 
     const navigate = useNavigate()
+
+    const {id} = useParams()
 
     const submitForm = (e) => {
         e.preventDefault()
 
-        const newJob = {
+        const updatedJob = {
+            id,
             title,
             type,
             location,
@@ -32,12 +38,13 @@ const AddJobPage = ({ addJobSubmit}) => {
             }
         }
 
-        addJobSubmit(newJob);
+        updateJobSubmit(updatedJob);
 
-        toast.success('Job added successfully')
+        toast.success('Job Updated successfully')
 
-        return navigate('/jobs');
+        return navigate(`/jobs/${id}`);
     }
+
 
   return (
     <section className="bg-indigo-50">
@@ -224,4 +231,4 @@ const AddJobPage = ({ addJobSubmit}) => {
   )
 }
 
-export default AddJobPage
+export default EditJobPage
